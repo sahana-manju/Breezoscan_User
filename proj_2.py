23112ce35146472e3299ee27ad84f3c4239eb5ac
sahana-manju <56404564+sahana-manju@users.noreply.github.com>
2023-12-06 00:42:52 -0500

    Update proj_2.py

--- proj_2.py
+++ proj_2.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import hiplot as hip
 from streamlit import runtime
 from math import radians, sin, cos, sqrt, atan2
 import geocoder
